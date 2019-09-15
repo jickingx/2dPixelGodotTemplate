78cd928e1bc6c4bc7f1d46c411b57d4294b995d4
jicking bebiro <jdbebiro@gmail.com>
2019-09-16 02:13:49 +0800

    Updated jumper
    Add dialoguebox
    - auto loaded by Global.gd
    - fetch data from json
    - requires player instance on scene

--- Global.gd
+++ Global.gd
@@ -6,6 +6,18 @@ var current_scene = null
 #scene transition
 var screen_overlay = null
 
+#dialogue
+const DIALOGUEBOX_NAME = "DialogueBox"
+var json_path = "res://simple-dialogue-box-data.json"
+var data = {}
+var player = null
+var dialoguebox = null
+
+#player states
+const PLAYER_NAME = "Jumper"
+var hasUnlockedLevel00 : bool = false
+var block00_position = null
+
 func _ready():
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() - 1)
@@ -17,8 +29,8 @@ func goto_scene(path):
 	# or some other function in the current scene.
 	# The solution is to defer the load to a later time, when
 	# we can be sure that no code from the current scene is running:
-	#load_json_data()
-	#setup_simple_dialogue()
+	load_json_data()
+	setup_simple_dialogue()
 	setup_fade_transition()
 	call_deferred("_deferred_goto_scene", path)
 
@@ -39,7 +51,7 @@ func _deferred_goto_scene(path):
 	# Optionally, to make it compatible with the SceneTree.change_scene() API.
 	get_tree().set_current_scene(current_scene)
 	#add dialogue to scene
-	#setup_simple_dialogue()
+	setup_simple_dialogue()
 	#add fade transition to scene
 	setup_fade_transition()
 
@@ -51,6 +63,64 @@ func setup_fade_transition():
 	current_scene.add_child(so)
 	screen_overlay = so
 
-func fade_out_transition():
+func fade_out_transition() -> void:
 	if screen_overlay != null :
 		screen_overlay.fade_out()
+
+func setup_simple_dialogue() -> void:
+	#Check if player in scene then add dialogue manager
+	#todo: refator to get player based on group
+	if current_scene.get_node(PLAYER_NAME) == null:
+		print_debug("No player")
+		player = null
+		dialoguebox = null
+		return
+	
+	print_debug("Has player")
+	#INTANTIATE DIALOGUEBOX
+	var dbox_path = load("res://UI/DialogueBox/DialogueBox.tscn")
+	var dbox = dbox_path.instance()
+	dbox.hide()
+	dbox.set_name(DIALOGUEBOX_NAME)
+	current_scene.add_child(dbox)
+	
+	player = current_scene.get_node(PLAYER_NAME)
+	dialoguebox = current_scene.get_node(DIALOGUEBOX_NAME)
+	
+	#TODO: refactor if needed to call npc_collided signal
+	#or call _on_Player_npc_collided directly
+	#ATTACH SIGNALS 
+	player.connect("npc_collided", self, "_on_Player_npc_collided")
+	dialoguebox.connect("closed", self, "_on_DialogueBox_closed")
+
+
+func show_simple_dialogue(npc_name):
+	var text = data[npc_name]
+	dialoguebox.dialogue_text = text
+	dialoguebox._ready()
+	dialoguebox.show()
+
+
+func _on_Player_npc_collided(npc_name):
+	print_debug("GLOBAL collided npc:" + npc_name)
+	player.disable()
+	show_simple_dialogue(npc_name)
+
+
+func _on_DialogueBox_closed():
+	player.enable()
+
+
+func load_json_data():
+	#READ JSON
+	var data_file = File.new()
+	if data_file.open(json_path, File.READ) != OK:
+		return
+	var data_text = data_file.get_as_text()
+	data_file.close()
+	var data_parse = JSON.parse(data_text)
+	if data_parse.error != OK:
+		return
+	data = data_parse.result
+	#print_debug(data["1"].name) #FOR JSON FORMAT WITH {"key":{"id":"value"}}
+	#print_debug(data["NpcNameHere"])
---
+++ UI/DialogueBox/DialogueBox.gd
@@ -0,0 +1,74 @@
+extends CanvasLayer
+signal closed
+
+var delta_time = 0
+export var speed = 0.1
+export(String, MULTILINE) var dialogue_text = ""
+export var confirm_action = "ui_accept"
+var current_page = 0
+var pages = []
+var rtLabel
+
+
+func _ready():
+	current_page = 0
+	pages = []
+	rtLabel = $Container/RichTextLabel
+	rtLabel.visible_characters = 0
+	rtLabel.bbcode_enabled = true
+	
+	parse_dialogue_text()
+	rtLabel.bbcode_text  = pages[current_page]
+	set_process_input(true)
+	set_process(true)
+
+
+func parse_dialogue_text():
+	pages = dialogue_text.split(";")
+	for i in range(pages.size()):
+		pages[i] = pages[i].replace("\n","")
+
+
+func text_advance():
+	if(rtLabel.text.length() == rtLabel.visible_characters):
+		current_page = current_page + 1
+		rtLabel.visible_characters = 0
+		delta_time = 0
+		if(pages.size() - 1 >= current_page):
+			rtLabel.bbcode_text = align_center( pages[current_page])
+		else:
+			$Container.visible = false
+			emit_signal("closed")
+
+
+func text_update():
+	if(rtLabel.text.length() > rtLabel.visible_characters):
+		rtLabel.visible_characters = rtLabel.visible_characters + 1
+
+
+func align_center(text):
+	var x = "[center]" + text + "[/center]"
+	return x
+
+
+func hide():
+	$Container.visible = false
+
+
+func show():
+	$Container.visible = true
+
+
+func _input(event):
+	if(event.is_action_released(confirm_action)):
+		if(rtLabel.visible_characters == rtLabel.text.length()):
+			text_advance()
+		else:
+			rtLabel.visible_characters = rtLabel.text.length()
+
+
+func _process(delta):
+	if(delta_time > speed):
+		delta_time = 0
+		text_update()
+	delta_time = delta_time + delta
